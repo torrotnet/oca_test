5fde3f3f3d65ef6a399e64f9c3d21e770e543f6d
Piellia Vasyl <pellia@ukr.net>
2015-09-15 16:55:01 +0300

    Add one test result.

--- oca_test/choices.py
+++ oca_test/choices.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 ANSWERFORM_CHOICES = (('1', 'Yes',), ('2', 'Do not know',), ('3', 'No'))
 
 QUESTION_CHOICES = [("A", "A"), ("B", "B"), ("C", "C"), ("D", "D"), ("E", "E"), ("F", "F"), ("G", "G"), ("H", "H"),
@@ -5,4 +6,4 @@ QUESTION_CHOICES = [("A", "A"), ("B", "B"), ("C", "C"), ("D", "D"), ("E", "E"), 
 
 USER_AGE_CHOICES = [(u'1', u"14-18"), (u'2', u"18+")]
 
-USER_SEX_CHOICES = [(u'1', u"female"), (u'2', u"male")]
+USER_SEX_CHOICES = [(u'1', u"Женский"), (u'2', u"Мужской")]
